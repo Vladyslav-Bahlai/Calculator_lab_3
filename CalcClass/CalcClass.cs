58f41aba9656d32cac5ef38a3b9ccbbce79bad8e
Igoryk <igorivanyk11@gmail.com>
2022-04-23 20:07:30 +0300

    created CalcClass module, added main arithmetic functions to it

---
+++ CalcClass/CalcClass.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace CalcClass
+{
+    public class CalcClass
+    {
+        static public int Add(long a, long b) {
+            return (int)(a + b);
+        }
+        static public int Sub(long a, long b) {
+            return (int)(a - b);
+        }
+        static public int Mult(long a, long b) {
+            return (int)(a * b);
+        }
+        static public int Div(long a, long b) {
+            return (int)(a / b);
+        }
+        static public int Mod(long a, long b) {
+            return (int)(a % b);
+        }
+        static public int ABS(long a) {
+            return (int)(+a);
+        }
+        static public int IABS(long a) {
+            return (int)(-a);
+        }
+    }
+}
